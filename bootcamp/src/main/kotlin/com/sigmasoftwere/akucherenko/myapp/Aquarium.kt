fcd787ad3b975991ef873d8637c795d31a296aad
Aleksandr Kucherenko <alexk2111@ukr.net>
2021-09-11 18:32:28 +0300

    create package and class

---
+++ bootcamp/src/main/kotlin/com/sigmasoftwere/akucherenko/myapp/Aquarium.kt
@@ -0,0 +1,12 @@
+package com.sigmasoftwere.akucherenko.myapp
+
+class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+
+    fun printSize() {
+        println(
+            "Width: $width cm " +
+                    "Length: $length cm " +
+                    "Height: $height cm "
+        )
+    }
+}
