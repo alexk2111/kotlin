1578654e6d356c5c685ca90047c1b60239372a6f
Aleksandr Kucherenko <alexk2111@ukr.net>
2021-09-13 13:29:29 +0300

    set class open and add new properties

--- bootcamp/src/main/kotlin/com/sigmasoftwere/akucherenko/myapp/Aquarium.kt
+++ bootcamp/src/main/kotlin/com/sigmasoftwere/akucherenko/myapp/Aquarium.kt
@@ -1,6 +1,6 @@
 package com.sigmasoftwere.akucherenko.myapp
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+open class Aquarium(open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
 
     init {
         println("aquarium initializing")
@@ -23,13 +23,19 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
                     "Length: $length cm " +
                     "Height: $height cm "
         )
+        println("Volume: $volume l Water: $water l (${water/volume*100.0}% full)")
     }
 
     fun volume() = width * height * length
 
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000
         set(value) {
             height = (value * 1000) / (width * length)
         }
+
+    open val shape = "rectangle"
+
+    open var water: Double = 0.0
+        get() = volume * 0.9
 }
